94f22d33da6180810674c6a2b541bf43c8c589d8
wunderlins <simon.wunderlin@usb.ch>
2016-03-07 07:59:03 +0100

    dev environment

--- REAMDE.md
+++ REAMDE.md
@@ -1,1 +1,6 @@
 # OIB Tafel Backend
+
+## install
+    $ virtualenv tafel
+    $ source ./tafel/bin/activate
+    $ pip install Django # django 1.9.4 used during development
---
+++ tafel/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wunderlins/Projects/oib-backend/tafel/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
